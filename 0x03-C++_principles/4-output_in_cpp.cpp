ce515b992062e39233c1dc0162f343cf626df109
Z-ajamy <abdorahman0283@gmail.com>
2024-02-04 19:54:24 +0200

    output

---
+++ 0x03-C++_principles/4-output_in_cpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(void)
+{
+    std::cout << "I love programming\n\nI promise to be the best devolper ever";
+    std::cout << "I know it will take some time to practice but I\nwill achieve my goal" << std::endl << std::endl;
+    std::cout << "Abdo Al-ajamy\n";
+
+}
