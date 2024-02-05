7aebbda907c146a6e03f49390f831e4035c2458b
Z-ajamy <abdorahman0283@gmail.com>
2024-02-05 20:41:57 +0200

    user_input

---
+++ 0x03-C++_principles/5-USER_INPUTS/2-user_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int age, age_after;
+    cout << "inter your age\n";
+    cin >> age ;
+    age_after = age + 5;
+    cout << "\nafter 5 years your age will be " << age_after << endl;
+}
