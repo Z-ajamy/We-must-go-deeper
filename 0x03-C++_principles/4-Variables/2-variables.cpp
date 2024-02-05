36d2fad7f6b5e1646f216929dccdcc36fc2973c5
Z-ajamy <abdorahman0283@gmail.com>
2024-02-05 19:03:46 +0200

    variables

---
+++ 0x03-C++_principles/4-Variables/2-variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+int --> 5
+char --> 'A'
+float --> 3.14
+double --> 3.15451258438
+bool --> true
+string --> "abdo"
+wchat_t --> "عبدالرحمن"
+*/
+int main ()
+{
+    int year = 21;
+    cout << "after 5 years my age will be " << year + 5 <<" years old." << endl;
+}
