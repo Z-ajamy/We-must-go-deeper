873ae7e0bb1006118640900348668ed0d4a0e175
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 17:28:17 +0200

    logical_operators

---
+++ 0x03-C++_principles/11_Logical_operators/17-logical_operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/**
+ * 
+ * || --> OR
+ * && --> AVD 
+ * ! --> NOT
+ * 
+*/
+int main ()
+{
+    bool a;
+    a = ((5 > 6 && !(7 <= 8)) && (8 > 1 || 4 <= 3) || true);
+    cout << "((5 > 6 && !(7 <= 8)) && (8 > 1 || 4 <= 3) || true) = " << a << endl ;
+    return 0;
+}
