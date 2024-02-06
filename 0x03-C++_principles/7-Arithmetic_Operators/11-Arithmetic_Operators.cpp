5b6928b9a01f1c9160dc607cb56545daad59318e
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 13:05:03 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/11-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/11-Arithmetic_Operators.cpp
@@ -15,5 +15,11 @@ using namespace std;
 
 int main ()
 {
-    
+    float num ;
+    cout << "Enter the number\n" ;
+    cin >> num ;
+    cout << "the power of the number " << num <<" is\n" << num * num 
+    << endl << num * num * num<< endl << 
+    num * num * num * num << endl;
+    return 0;
 }
