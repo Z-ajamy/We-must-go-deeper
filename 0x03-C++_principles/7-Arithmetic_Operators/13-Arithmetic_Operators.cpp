e1a6c33759618989f78a4506171e39234b9723a6
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 13:28:31 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/13-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/13-Arithmetic_Operators.cpp
@@ -15,5 +15,12 @@ using namespace std;
 
 int main ()
 {
-    
+    float num1 , num2  ;
+    cout << "Enter the invoice value\n" ;
+    cin >> num1 ;
+    cout << "Enter what the customer paid\n" ;
+    cin >> num2 ;
+    cout << "__________________\nThe rest of the amount is " << num2 - num1 << endl ;
+
+    return 0;
 }
