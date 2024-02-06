e59da54a08c30aa8b5467980933653865f68cc14
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 12:33:55 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/7-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/7-Arithmetic_Operators.cpp
@@ -15,5 +15,10 @@ using namespace std;
 
 int main ()
 {
-    
+    float Diameter , Area , PI = 3.14;
+    cout << "Enter the diameter of the circle\n" ;
+    cin >> Diameter;
+    Area = PI * Diameter * Diameter * (1.0/4) ;
+    cout << "the area of circle is " << Area << endl;
+return 0;
 }
