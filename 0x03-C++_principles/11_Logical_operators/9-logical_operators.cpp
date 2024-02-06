24bfa816206c8a2c05c5e2c0fbd5157cb6635ef7
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 17:12:49 +0200

    logical_operators

---
+++ 0x03-C++_principles/11_Logical_operators/9-logical_operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+/**
+ * 
+ * || --> OR
+ * && --> AVD 
+ * ! --> NOT
+ * 
+*/
+int main ()
+{
+    bool a;
+   a = (7 > 7 || 7 > 5);
+    cout << "(7 > 7 || 7 > 5) = " << a << endl ;
+}
