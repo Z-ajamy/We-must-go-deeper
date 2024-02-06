e71ae589d6f33697645822da09c4b3e49e0dd1d1
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 13:48:43 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/15-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/15-Arithmetic_Operators.cpp
@@ -15,5 +15,20 @@ using namespace std;
 
 int main ()
 {
+    float days , hours , Minutes , Seconds ;
+
+    cout << "Enter the time you spent on this task\n" ;
+    cout << "by days ";
+    cin >> days;
+    cout << "by hours ";
+    cin >> hours;
+    cout << "by Minutes ";
+    cin >> Minutes;
+    cout << "by Seconds ";
+    cin >> Seconds;
     
+    Seconds = Seconds + Minutes * 60 + hours * 60 * 60 + days * 60 * 60 * 24;
+
+     cout << "__________________\nthe time you spent on this task is\n" 
+    << Seconds << " Seconds\n" << endl ;
 }
