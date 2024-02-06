aa73a55ff63935b88b98be7e792528486c4f2c47
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 13:21:35 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/12-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/12-Arithmetic_Operators.cpp
@@ -15,5 +15,25 @@ using namespace std;
 
 int main ()
 {
-    
+    float penny , nickle , dime , quarter , dollar;
+
+    cout << "Enter what is in the piggy bank\n" ;
+    cout << "by penny ";
+    cin >> penny;
+    cout << "by nickle ";
+    cin >> nickle;
+    cout << "by dime ";
+    cin >> dime;
+    cout << "by quarter ";
+    cin >> quarter;
+    cout << "by dollar ";
+    cin >> dollar;
+
+    penny = penny + nickle * 5 + dime * 10 + quarter * 25 + dollar *100 ;
+    dollar = (penny / 100.0) ;
+
+    cout << "__________________\nThe one in the piggy bank is\n" 
+    << penny << " pennys\n" << dollar << 
+    " dollars" << endl;
+    return 0;
 }
