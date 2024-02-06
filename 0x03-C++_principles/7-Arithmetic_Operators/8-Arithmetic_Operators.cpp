a61a0aad2cc57362fd282e19850a188d78289f25
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 12:37:24 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/8-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/8-Arithmetic_Operators.cpp
@@ -15,5 +15,10 @@ using namespace std;
 
 int main ()
 {
-    
+    float length , Area , PI = 3.14;
+    cout << "To calculate the area of ​​a circle inside a square, give the side length of the square\n" ;
+    cin >> length;
+    Area = PI * length * length * (1.0/4) ;
+    cout << "the area of circle is " << Area << endl;
+return 0;
 }
