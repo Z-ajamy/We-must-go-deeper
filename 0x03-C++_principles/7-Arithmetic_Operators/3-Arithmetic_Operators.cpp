f6d8d9526bc241ee65c7c6c4bcefaf3040e1e5b8
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 02:18:40 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/3-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/3-Arithmetic_Operators.cpp
@@ -15,5 +15,12 @@ using namespace std;
 
 int main ()
 {
-    
+    float num1 , num2 , num3;
+    cout <<"inter the 3 numbers\n";
+    cin >> num1 >> num2 >> num3;
+    cout << "________________________\nthe average of " 
+    << num1 <<" and " << num2 
+    << " and " << num3 << " is " 
+    << (num1 + num2 + num3)/3 
+    << endl;
 }
