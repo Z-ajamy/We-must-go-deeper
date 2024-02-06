da66d3fed02dcef0267ce5f7a30acab46e68ea31
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 12:59:09 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/10-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/10-Arithmetic_Operators.cpp
@@ -15,5 +15,10 @@ using namespace std;
 
 int main ()
 {
-    
+        float base , side_length , Area , PI = 3.14;
+    cout << "To calculate the area of ​​a circle enclosed in an isosceles triangle, enter the base length and the side length.\n" ;
+    cin >> side_length >> base;
+    Area = (PI * base * base) * (1.0/4) * (2 * side_length - base) / (2 * side_length + base) ;
+    cout << "the area of circle is " << Area << endl;
+return 0;
 }
