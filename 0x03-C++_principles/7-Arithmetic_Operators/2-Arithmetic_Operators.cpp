204d98642178ce0cf4c4766616ee5092f63f5399
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 02:09:30 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/2-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/2-Arithmetic_Operators.cpp
@@ -16,4 +16,11 @@ using namespace std;
 int main ()
 {
     
+    float num1 , num2 , num3;
+    cout <<"inter the 3 numbers\n";
+    cin >> num1 >> num2 >> num3;
+    cout << "the sum of " << num1 << 
+    " + " << num2 << " + " << num3 <<
+    " is " << num1 + num2 + num3 << endl;
+
 }
