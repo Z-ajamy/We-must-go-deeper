e0c5ffeae015c3428c92b65d165ebf2f7ce04d36
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 12:27:31 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/6-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/6-Arithmetic_Operators.cpp
@@ -15,5 +15,10 @@ using namespace std;
 
 int main ()
 {
-    
+    float height , base , area ;
+    cout << "Enter the base and height of the triangle\n" ;
+    cin >> base >> height ;
+    area = base * height * (1.0/2);
+    cout << "the area of triangle is " << area << endl;
+    return 0;
 }
