a605fbe663e4edd703399cd994bde81f207ffab3
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 14:30:58 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/18-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/18-Arithmetic_Operators.cpp
@@ -16,4 +16,15 @@ using namespace std;
 int main ()
 {
     
+    unsigned int loan , months , pay_per_month ;
+
+    cout << "Enter the loan amount\n" ;
+    cin >> loan;
+    cout << "Enter the number of months you need to expires\n" ;
+    cin >> pay_per_month ;
+
+    months = loan / pay_per_month;
+
+    cout << "_______________________\nyou need " 
+    << months << " months to expires" <<endl ;
 }
