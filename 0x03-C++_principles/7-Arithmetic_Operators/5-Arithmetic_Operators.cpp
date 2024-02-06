02bd9248e374b6ec4606065460f5e10a74b28833
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 02:31:45 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/5-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/5-Arithmetic_Operators.cpp
@@ -15,5 +15,11 @@ using namespace std;
 
 int main ()
 {
-    
+    unsigned short int num1 , num2 ;
+    cout << "give me the The length of the rectangle\n";
+    cin >> num1;
+    cout << "give me the The Width of the rectangle\n";
+    cin >> num2;
+    cout << "the rectangle area is " << num1 * num2 << endl;
+return 0;
 }
