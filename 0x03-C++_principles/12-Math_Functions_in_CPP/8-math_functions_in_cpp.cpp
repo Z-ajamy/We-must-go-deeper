0b5377fb3e074ae52c82e1119e506cb9bef21b06
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 22:07:00 +0200

    math_functions

---
+++ 0x03-C++_principles/12-Math_Functions_in_CPP/8-math_functions_in_cpp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+/**
+ * 
+ * #include <cmath>
+ *  
+ * float A.2 , B 
+ * sqrt(A.2) ---> (A.2)^(1/2)
+ * round(A.2) ---> A
+ * pow(A , B) ---> (A)^(B)
+ * ceil(A,1) ---> A + 1
+ * floor(A,9) ---> A
+ * abs(-A) ---> A
+ * 
+*/
+int main ()
+{
+    float num , p , r;
+
+    cout << "enter the numbrt\n";
+    cin >> num;
+    cout << "enter the power\n";
+    cin >> p;
+
+
+    r = pow(num , p);
+
+    cout << "__________________\n" << r << endl;
+    return 0;
+}
