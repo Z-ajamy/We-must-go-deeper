075b0157c1b06abf7dbaf0bdb2a8748e3a6b1059
Z-ajamy <abdorahman0283@gmail.com>
2024-02-06 12:41:55 +0200

    Arithmetic_Operators

--- 0x03-C++_principles/7-Arithmetic_Operators/9-Arithmetic_Operators.cpp
+++ 0x03-C++_principles/7-Arithmetic_Operators/9-Arithmetic_Operators.cpp
@@ -15,5 +15,10 @@ using namespace std;
 
 int main ()
 {
-    
+        float circumference , Area , PI = 3.14;
+    cout << "To calculate the area of ​​a circle using its circumference, enter its circumference\n" ;
+    cin >> circumference;
+    Area = circumference * circumference / (PI*4) ;
+    cout << "the area of circle is " << Area << endl;
+return 0;
 }
