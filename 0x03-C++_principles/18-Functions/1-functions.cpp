490f655840d6068e3c0f034f88a6e8a839876cb8
Z-ajamy <abdorahman0283@gmail.com>
2024-02-08 19:41:49 +0200

    Functions

---
+++ 0x03-C++_principles/18-Functions/1-functions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * 
+ * type fun_name ()
+ *  {
+ *      code
+ *  }
+ * 
+ * int main ()
+ *  {
+ *      fun_name();  ----> call the function
+ *  }
+ * 
+*/
+
+void fun_name (void)
+{
+    std::cout << "************\n";
+    std::cout << "************\n";
+    std::cout << "************\n";
+    std::cout << "************\n";
+    std::cout << "************" << std::endl;
+}
+
+#include <iostream>
+#include <iostream>
+int main(void)
+{
+    fun_name();
+    return 0;
+}
