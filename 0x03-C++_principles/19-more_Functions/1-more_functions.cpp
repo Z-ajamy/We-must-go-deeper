2b488666b57acf28f8ec4b6c0284daf47ebb3861
Z-ajamy <abdorahman0283@gmail.com>
2024-02-09 13:58:05 +0200

    more_functions

--- 0x03-C++_principles/19-more_Functions/1-more_functions.cpp
+++ 0x03-C++_principles/19-more_Functions/1-more_functions.cpp
@@ -3,14 +3,36 @@ using namespace std;
 
 /**
  * 
- * type fun_name ()
+ * type_return fun_name ()
  *  {
  *      code
+ *      return value;
  *  }
  * 
  * int main ()
  *  {
- *      fun_name();  ----> call the function
+ *      fun_name();  ----> call the function to return the value
  *  }
  * 
+ * void fun_name () -----> procedure
+ * 
+ * type_return fun_name () -----> function
+ * 
 */
+
+int function_sum ()
+{
+     int a , b , r;
+    cout << "inter your number" << endl ;
+    cin >> a;
+    cout << "inter your second number" << endl ;
+    cin >> b;
+    r = a + b;
+    return (r) ;
+}
+int main ()
+{   int r;
+    r = function_sum() ;
+    cout << "______________\n" << r << endl ;
+
+}
