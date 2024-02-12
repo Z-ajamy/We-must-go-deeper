52efe9f9fe612e25bf15ec4bfe0b00cfbb8a24c5
Z-ajamy <abdorahman0283@gmail.com>
2024-02-12 18:23:47 +0200

    For_Loops

---
+++ 0x03-C++_principles/30-For_Loops/2-For_Loops.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+/**
+ * 
+ *          for (initialization ; condition ; update)  ----true 
+ *          ^                                                  |
+ *          |--- {body}     <----------------------------------
+ * 
+ */
+int main ()
+{
+    short int j , a = 0;
+    cin >> j ;
+    for (int i = 1; i <= j; i++)
+    {
+        if ((i % 2) != 0)
+        {
+            a += i;
+        }
+    }
+    cout << a << " \n";
+
+}
