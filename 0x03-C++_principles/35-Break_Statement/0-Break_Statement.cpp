1196124b648935d9731ff6707317b16e267d9bf4
Z-ajamy <abdorahman0283@gmail.com>
2024-02-16 22:29:15 +0200

    Break_Statement

---
+++ 0x03-C++_principles/35-Break_Statement/0-Break_Statement.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+/**
+ * 
+ * int i; -------> initialization 
+ * 
+ * while (condition)
+ *  {
+ *      body;
+ *  }
+ * 
+*/
+
+int main ()
+{
+    int lengh , num , i;
+    int arr[100];
+
+    cout << "enter number of numbers you need" << endl;
+    cin >> lengh;
+    cout << "enter the numbers\n";
+    for (i = 0; i < lengh; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the number whose order you want to know\n";
+    cin >> num;
+    for (i = 0; i < lengh; i++)
+    {
+        if (arr[i] == num)
+        {
+            break;
+        }
+    }
+    cout << "The order of entry was " << (i + 1) << endl;
+    return 0;
+}
