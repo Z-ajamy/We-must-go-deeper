a62921d31ae73bba44d7acc6822913e78cb0bb0a
Z-ajamy <abdorahman0283@gmail.com>
2024-02-16 16:27:57 +0200

    Nested_For_Loops

---
+++ 0x03-C++_principles/32-Nested_For_Loops/3-Nested_For_Loops.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/**
+ *
+ *          for (int i = 0; i < Length; i++)
+ *              {
+ *                  for (int j = 0; j < Length; j++)
+ *                      {
+ *                          {body1}
+ *                      }
+ *                  {body2}
+ *              }
+ *
+ */
+
+int main ()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
