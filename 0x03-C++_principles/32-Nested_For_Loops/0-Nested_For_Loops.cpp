bf5e6b5bd9bb65e87cb7069c799b560e91d355a7
Z-ajamy <abdorahman0283@gmail.com>
2024-02-16 16:15:27 +0200

    Nested_For_Loops

--- 0x03-C++_principles/32-Nested_For_Loops/0-Nested_For_Loops.cpp
+++ 0x03-C++_principles/32-Nested_For_Loops/0-Nested_For_Loops.cpp
@@ -14,13 +14,13 @@ using namespace std;
  *
  */
 
-int main ()
+int main()
 {
     for (int i = 0; i <= 12; i++)
     {
         for (int j = 0; j <= 12; j++)
         {
-            cout << i << " * " << j << " = " << i *j << endl;
+            cout << i << " * " << j << " = " << i * j << endl;
         }
         if (i != 12)
         {
