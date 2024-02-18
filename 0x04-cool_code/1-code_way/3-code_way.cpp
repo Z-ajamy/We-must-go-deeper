ee1253aa947a5413d45af0ca20dd501f6892030e
Z-ajamy <abdorahman0283@gmail.com>
2024-02-18 14:33:12 +0200

    code_way

---
+++ 0x04-cool_code/1-code_way/3-code_way.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct strinfo
+{
+    short int age;
+    bool drive_licance;
+};
+
+struct strinfo read_info ()
+{
+    struct strinfo info;
+
+    cout << "enter your age\n";
+    cin >> info.age;
+    cout << "do you have drive licance ? (n = 0)\n";
+    cin >> info.drive_licance;
+    return info;
+}
+bool is_accepted (struct strinfo info)
+{
+    return (info.age >= 18 && info.drive_licance == 1);
+}
+void print_result (struct strinfo info)
+{
+    if (is_accepted(info))
+    {
+        cout << "accepted\n";
+    }
+    else
+    cout << "Un-accepted\n";
+}
+
+int main ()
+{
+    print_result(read_info());
+}
