f695cf8d935201eb258b5f36ccc3430ed9e3ac92
Z-ajamy <abdorahman0283@gmail.com>
2024-02-18 22:21:09 +0200

    code_way

---
+++ 0x04-cool_code/1-code_way/8-code_way.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+void read_num(int &num1 , int &num2 , int &num3)
+{
+    cout << "enter your first number\n";
+    cin >> num1;
+    cout << "enter your second number\n";
+    cin >> num2;
+    cout << "enter your third number\n";
+    cin >> num3;
+}
+
+int sum (int num1 , int num2 , int num3)
+{
+    return (num1 + num2 + num3) ;
+}
+
+void print (int num1 ,int num2 ,int num3)
+{
+    cout << "the result of " << num1 << " + " << num2 <<
+    " + " << num3 << " = " << sum(num1 , num2 , num3) << endl;
+}
+
+int main ()
+{
+    int num1 , num2 , num3;
+    read_num(num1 , num2 , num3);
+    print(num1 , num2 , num3);
+    return 0;
+}
